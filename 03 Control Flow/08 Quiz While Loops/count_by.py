441d8ac4263274b820ca1c374a1086ad5ebd91ab
Andrey Yohanes <andrey.yohanes@gmail.com>
2021-03-07 19:26:27 +0700

    Create count_by.py

---
+++ 03 Control Flow/08 Quiz While Loops/count_by.py
@@ -0,0 +1,12 @@
+start_num = 5
+end_num = 100
+count_by = 13
+
+# write a while loop that uses break_num as the ongoing number to 
+#   check against end_num
+break_num = start_num
+
+while break_num < end_num:
+    break_num += count_by
+
+print(break_num)
