d10d6fa537aebf8d1bc3837a00c9a6c5975dce50
Andrey Yohanes <andrey.yohanes@gmail.com>
2022-02-14 21:09:38 +0700

    Create chunker.py

---
+++ 06 Advanced Topics/chunker.py
@@ -0,0 +1,7 @@
+def chunker(iterable, size):
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+        
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
